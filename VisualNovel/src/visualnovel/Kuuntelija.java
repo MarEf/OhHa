c316372d44daf098547c280cb17dfb88cadf26b9
Maria Efimova <maef@dhp-62.cs.helsinki.fi>
2013-01-31 14:13:26 +0200

    etenee(kö)

---
+++ VisualNovel/src/visualnovel/Kuuntelija.java
@@ -0,0 +1,21 @@
+
+package visualnovel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author maef
+ */
+public class Kuuntelija implements ActionListener{
+
+    public Kuuntelija() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- VisualNovel/src/visualnovel/Selaaja.java
+++ VisualNovel/src/visualnovel/Selaaja.java
@@ -8,8 +8,23 @@ import visualnovel.ikkunat.*;
  * @author maef
  */
 public class Selaaja {
-    private int i;
-    private int j;
-    private Ikkuna ikkuna;
+    private int i = 0;
+    private int j = 0;
+    private Ikkunat ikkunat;
+    private Ikkuna[][] taulukko;
+
+    public Selaaja(Ikkunat ikkunat) {
+        this.ikkunat = ikkunat;
+        taulukko = ikkunat.getIkkunat();
+    }
+    
+    public void seuraava() {
+        
+    }
+    
+    public Ikkuna palautaIkkuna(int i, int j) {
+        return taulukko[i][j];
+    }
+    
     
 }
